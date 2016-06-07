4215f43ff4acf36efb9677477aea9b2550bef6f5
davide940 <xbox940@gmail.com>
2016-06-07 18:02:44 +0200

    Update

--- Oscilloscope/nbproject/Makefile-variables.mk
+++ Oscilloscope/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Oscilloscope.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Oscilloscope.production.hex
+CND_ARTIFACT_NAME_default=lcd.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lcd.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=oscilloscope.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/oscilloscope.tar
+CND_PACKAGE_NAME_default=lcd.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lcd.x.tar
